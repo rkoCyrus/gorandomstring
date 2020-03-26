cf7868bcb5ef942ac7d1bd776f0c4ad5d42c4884
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-03-26 16:13:00 +0800

    Update gorandomstring.go

--- gorandomstring.go
+++ gorandomstring.go
@@ -1,6 +1,9 @@
 package gorandomstring
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var validChar = [55]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'r', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
