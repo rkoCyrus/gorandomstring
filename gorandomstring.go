f2f2768722cd450630d439cbb7b741381a75bcc7
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-03-26 15:57:53 +0800

    Update gorandomstring.go

--- gorandomstring.go
+++ gorandomstring.go
@@ -4,19 +4,20 @@ import "math/rand"
 
 var validChar = [55]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'r', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
-func GenString(charLength int) string {
+func GenString(charLength int, repeatChar bool) string {
 	var result string
 	for letter := 0; letter < charLength; letter++ {
 		prePick := 0
 		for {
 			picker := rand.Int() % 55
-			if picker == prePick {
+			if picker == prePick && !repeatChar {
 				continue
 			} else {
 				result += string(validChar[picker])
+				prePick = picker
 				break
 			}
 		}
 	}
-	return string(result)
+	return result
 }
