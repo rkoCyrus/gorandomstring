ebf1476dbc9dc94d44fc2b8c1e819659643d8045
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-03-31 21:56:25 +0800

    Add go.doc

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+	This is first test of Go package
+	Which I'm try to figure out how it work
+
+	This package is generating random charather without highly simular charther:
+	I and l/1
+	0 and o/O
+*/
+
+package gorandomstring
